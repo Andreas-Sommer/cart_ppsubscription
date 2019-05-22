ae99947bfa18b6e708c300e7473daceb3e2182de
Andreas Sommer <sommer@belsignum.com>
2019-05-22 17:33:05 +0200

    add more fields for subscription tab

--- Classes/Domain/Model/Product/Product.php
+++ Classes/Domain/Model/Product/Product.php
@@ -10,13 +10,40 @@ namespace Belsignum\PaypalSubscription\Domain\Model\Product;
 
 class Product extends \Extcode\CartProducts\Domain\Model\Product\Product
 {
-
 	/**
 	 * isSubscription
 	 *
 	 * @var bool
 	 */
 	protected $isSubscription = FALSE;
+
+	/**
+	 * paypalType
+	 *
+	 * @var string
+	 */
+	protected $paypalType = '';
+
+	/**
+	 * paypalCategory
+	 *
+	 * @var string
+	 */
+	protected $paypalCategory = '';
+
+	/**
+	 * paypalProductId
+	 *
+	 * @var string
+	 */
+	protected $paypalProductId = '';
+
+	/**
+	 * paypalPlanId
+	 *
+	 * @var string
+	 */
+	protected $paypalPlanId = '';
 
 	/**
 	 * @return bool
@@ -33,6 +60,71 @@ class Product extends \Extcode\CartProducts\Domain\Model\Product\Product
 	{
 		$this->isSubscription = $isSubscription;
 	}
+
+	/**
+	 * @return string
+	 */
+	public function getPaypalType(): string
+	{
+		return $this->paypalType;
+	}
+
+	/**
+	 * @param string $paypalType
+	 */
+	public function setPaypalType(string $paypalType): void
+	{
+		$this->paypalType = $paypalType;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPaypalCategory(): string
+	{
+		return $this->paypalCategory;
+	}
+
+	/**
+	 * @param string $paypalCategory
+	 */
+	public function setPaypalCategory(string $paypalCategory): void
+	{
+		$this->paypalCategory = $paypalCategory;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPaypalProductId(): string
+	{
+		return $this->paypalProductId;
+	}
+
+	/**
+	 * @param string $paypalProductId
+	 */
+	public function setPaypalProductId(string $paypalProductId): void
+	{
+		$this->paypalProductId = $paypalProductId;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPaypalPlanId(): string
+	{
+		return $this->paypalPlanId;
+	}
+
+	/**
+	 * @param string $paypalPlanId
+	 */
+	public function setPaypalPlanId(string $paypalPlanId): void
+	{
+		$this->paypalPlanId = $paypalPlanId;
+	}
+
 
 
 }
--- ext_tables.sql
+++ ext_tables.sql
@@ -3,4 +3,8 @@
 #
 CREATE TABLE tx_cartproducts_domain_model_product_product (
 	is_subscription smallint(6) unsigned DEFAULT '0' NOT NULL,
+	paypal_type varchar(255) DEFAULT '' NOT NULL,
+	paypal_category varchar(255) DEFAULT '' NOT NULL,
+	paypal_product_id varchar(255) DEFAULT '' NOT NULL,
+	paypal_plan_id varchar(255) DEFAULT '' NOT NULL,
 );
