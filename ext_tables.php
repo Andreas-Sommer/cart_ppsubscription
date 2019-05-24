6221787ca55d2d26dbe13a2244dbbfec4f387f5d
Andreas Sommer <sommer@belsignum.com>
2019-05-24 11:35:02 +0200

    Add Paypal Icon

--- ext_tables.php
+++ ext_tables.php
@@ -6,6 +6,11 @@
  * Time: 11:16
  */
 
+defined('TYPO3_MODE') or die();
+
+$iconPath = 'EXT:paypal_subscription/Resources/Public/Icons/';
+$_LLL_db = 'LLL:EXT:paypal_subscription/Resources/Private/Language/locallang_db.xlf:';
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
 	'Belsignum.paypal_subscription',
 	'Pi1',
